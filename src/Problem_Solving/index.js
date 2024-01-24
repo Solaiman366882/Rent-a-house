a30931f9e038d2fde4943efbc353f533156fdb57
Md.Solaiman <md.solaiman366882@gmail.com>
2024-01-24 12:11:02 +0600

    problem solving added to new js file

---
+++ src/Problem_Solving/index.js
@@ -0,0 +1,23 @@
+function isPalindrome(str){
+    let flag = true;
+    let Str = "";
+
+    for(let i  = 0; i < str.length; i++)
+    {
+        if((str[i] >= 'a' && str[i]<= 'z') || (str[i]>= 'A' && str[i] <= 'Z') || (str[i]) >= '0' && str[i] <= '9')
+        {
+            Str += str[i];
+        }
+    }
+    const newStr = Str.toLowerCase();
+    for(let i = 0,j = newStr.length -1; i < (newStr.length-1)/2; i++,j--)
+    {
+        if(newStr[i] != newStr[j])
+        {
+            flag = false;
+            break;
+        }
+    }
+    if(flag) return true;
+    else return false;
+}
